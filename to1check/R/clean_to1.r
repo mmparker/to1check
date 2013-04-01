65c2f916777c6ffc2ecbbab03be4aba8fb29446d
Matt Parker <mattmparker@gmail.com>
2013-04-01 10:20:41 -0600

    Keep the nice-names, drop the questionnaire-number-names
    The extract includes headers on two rows - the first row is nice-names,
    the second are the original names (based on the questionnaire numbers).
    I need all this nonsense to drop the second row, unfortunately.

--- to1check/R/clean_to1.r
+++ to1check/R/clean_to1.r
@@ -11,24 +11,41 @@ clean_to1 <- function(extractdir, outdir) {
     extracts <- list.files(extractdir,
                            pattern = "*.csv",
                            full.names = TRUE)
-    
-    originals <- lapply(extracts, read.csv)
-    
-    
+
+    # The DMS downloads have a header on two rows - row one is nice names,
+    # row two is original questionnaire-number names.
+    # This is the best way I can think of to keep the first and drop the
+    # second...
+    originals <- lapply(extracts, function(x) {
+
+        # Load in the data without headers
+        records <- read.csv(x, skip = 2)
+
+        # Get names from the same files
+        headers <- read.csv(x, nrows = 1)
+
+        names(records) <- names(headers)
+
+        records
+
+        }
+    )
+
+
     # Rename the entries in originals for ease of reference
     names(originals) <- tolower(gsub(x = basename(extracts),
                                      pattern = "^v(\\w*).*\\.csv",
                                      replace = "\\1")
     )
-    
-    
+
+
     # Check that all of the table extracts are present
     expectedtables <- c("followupfortb", "ltbi", "ltbifollowup", 
                         "master", "medicalhistory", "preenrollment", 
                         "qft", "riskfactor", "skintest", "tspot")
-    
+
     missingtables <- expectedtables[!expectedtables %in% names(originals)]
-    
+
     if(length(missingtables) > 0) {
         stop(paste(
             "The following tables are missing or didn't import correctly: ",
@@ -38,32 +55,32 @@ clean_to1 <- function(extractdir, outdir) {
             call. = FALSE
         )
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     # Rename the many, many variables to something readable
     renamed <- rename(originals)
-    
+
     # Convert variables to the correct type - e.g., char datetimes to POSIXct
     converted <- convert(renamed)
-    
+
     # Recode variables as needed
     recoded <- recode(converted)
-    
+
     # Add StudyId to every table.  As a general rule, though, 
     # StudyId is for display and PatientID remains the table key of record
     # (because that's how the DB is set up)
     mixed <- mix(recoded)
-    
-    
-    
+
+
+
     # Create a final "cleaned" dataset (preserves a consistent name
     # if something comes in after mixed
     to1clean <- mixed
-    
-    
+
+
     # Write the end result out for ease of reference
     save(to1clean, file = file.path(outdir, "to1clean.rdata"))
 
