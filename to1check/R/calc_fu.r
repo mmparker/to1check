69c8551fc1adc8cbeaaa3540a60a6caa5c372581
Matt Parker <mattmparker@gmail.com>
2013-04-01 17:44:46 -0600

    FU-calculating function.

--- to1check/R/calc_fu.r
+++ to1check/R/calc_fu.r
@@ -1,23 +1,49 @@
 
+# Report out participants who are eligible for follow-ups
 
 
-calc_fu <- function(x) {
+calc_fu <- function(cleanlist) {
 
     require(lubridate)
+    require(plyr)
 
-    # Test that x is a Date (or can be coerced)
+    # Subset to those with 1+ positive tests who completed enrollment
+    # This is a crude approximation
+    testpos <- subset(cleanlist$master, 
+                      subset = CloseReason %in% "Open",
+                      select = c("StudyId", "VisitDate"))
 
-    # Set up the results
-    fu.dates <- data.frame(enroll.date = x,
-                           start6 = x + months(6) - days(14),
-                           end6 = x + months(6) + days(30),
-                           start12 = x + months(12) - days(14),
-                           end12 = x + months(12) + days(30),
-                           start18 = x + months(18) - days(14),
-                           end18 = x + months(18) + days(30),
-                           start24 = x + months(24) - days(14),
-                           end24 = x + months(24) + days(30))
+    # Use visit date as the date of record - the protocol allows some 
+    # variability, but this ought to be close enough.
+    fu.months <- c(6, 12, 18, 24)
 
-    fu.dates
+
+    # Iterate over each possible month of FU, then bind them all together
+    fu.report <- ldply(fu.months, .fun = function(fu.month) {
+
+        # Load the test-positive
+        parts <- testpos
+
+        # Mark the start of their follow-up period
+        parts$fu_start <- parts$VisitDate + months(fu.month) - days(14)
+
+        # Mark the end of their follow-up period
+        parts$fu_end <- parts$VisitDate + months(fu.month) + days(30)
+
+        # If today is inside those dates, they're eligible for FU
+        parts$eligible <- Sys.Date() >= parts$fu_start &
+                            Sys.Date() <= parts$fu_end
+
+        # Add an indicator for which FU this is
+        parts$which_fu <- paste(fu.month, "-month FU", sep = "")
+                       
+        # Return only those who are eligible
+        subset(parts, eligible %in% TRUE)
+
+    })
+
+
+    # Return
+    fu.report
 
 }
