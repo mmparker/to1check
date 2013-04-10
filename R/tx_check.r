dcd7b833cd569940d6e48d3bb9ccf8ea594b0129
Matt Parker <mattmparker@gmail.com>
2013-04-10 16:51:26 -0600

    Reorganizing the entire project.

---
+++ R/tx_check.r
@@ -0,0 +1,12 @@
+
+
+
+# This function checks local electronic medical records for treatments
+# that aren't documented in DMS
+
+
+tx_check <- function(cleanlist, site) {
+
+    tx_check_denver(cleanlist, site)
+
+}
---
+++ R/tx_check_denver.r
@@ -0,0 +1,14 @@
+
+
+
+
+# This function checks TBdb for treatments that aren't documented in DMS
+
+
+tx_check_denver <- function(cleanlist, site) {
+
+    # Identify test-positive patients
+
+    # Load treatment records from TBdb
+
+}
