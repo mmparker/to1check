c44116f5738b8fa3b0842a292026aaf103cbb642
Matt Parker <mattmparker@gmail.com>
2013-04-10 19:01:01 -0600

    Add documentation for the country_codes.

---
+++ R/country_codes.r
@@ -0,0 +1,18 @@
+#' Countries by name and their ISO 3166-1 alpha-3 codes.
+#' 
+#' A dataset containing the names and three-letter ISO country codes of
+#' 249 countries, dependent territories, and special areas of geographical
+#' interest.
+#' 
+#' \itemize{
+#'   \item name. Name of country/area.
+#'   \item code ISO 3166-1 alpha-3 code for the country/area.
+#'   ...
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name country_codes
+#' @source \url{http://en.wikipedia.org/wiki/ISO_3166-1_alpha-3}
+NULL
+
